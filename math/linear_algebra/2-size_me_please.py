204569144f06247d8e423b915542e57c183e2ee6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-21 23:55:48 -0500

    create task2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
-def matrix_shape(matrix):
-    """
+""" 
     Calculates the shape of a matrix.
-
-    Args:
-        matrix: a nested list representing a matrix
-
-    Returns:
-        a list of integers representing the shape of the matrix
-    """
+    Args: matrix: a nested list representing a matrix
+    Returns: list 
+"""
+def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
