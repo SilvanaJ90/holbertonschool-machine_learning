b4b1ef5a32872adeb453c61369c30098e1274c85
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-22 00:15:46 -0500

    create-task3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import numpy as np
 """
-    Calculates the shape of a matrix.
-    Args: matrix: a nested list representing a matrix
-    Returns: list
+    Transpose of a 2D matrix, matrix
+    Args: matrix: a nested transponse a matrix
+    Returns: new matrix transponse
 """
 
 
 def matrix_transpose(matrix):
-    """ Calculates the shape of a matrix. """
-    transponse = np.transpose(matrix)
-    return transponse
+    """ Transpose of a 2D matrix, matrix """
+    row = len(matrix)
+    columns = len(matrix[0])
+    
+    """Create an empty matrix with changed dimensions"""
+    transposed_matrix = [[0 for j in range(row)] for i in range(columns)]
+    
+    
+    """Swaps each element of the matrix with its corresponding element in the opposite position."""
+    for i in range(row):
+        for j in range(columns):
+            transposed_matrix[j][i] = matrix[i][j]
+    
+    return transposed_matrix
