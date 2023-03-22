3cb69aa22f827d8f49fa5f42dc7c17419883af89
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-22 00:03:21 -0500

    update

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+    Calculates the shape of a matrix.
+    Args: matrix: a nested list representing a matrix
+    Returns: list
+"""
+
+
+def matrix_transpose(matrix):
+    """ Calculates the shape of a matrix. """
+    transponse = np.transpose(matrix)
+    return transponse
