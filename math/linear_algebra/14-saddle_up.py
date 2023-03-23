187ebb82dcd586942065935bea4662e98b1b66af
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-23 18:02:04 -0500

    create-task14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Performs matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Performs matrix multiplication """
+    return np.matmul(mat1, mat2)
