9a3b64968c535e7014ef202ea94257e6f0442930
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-23 18:32:32 -0500

    create-task8

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -9,13 +9,10 @@
 def mat_mul(mat1, mat2):
     """   Multiplication: two matrices element-wise """
     if len(mat1[0]) != len(mat2):
-            return None
-        
+        return None
     result = [[0 for j in range(len(mat2[0]))] for i in range(len(mat1))]
-    
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
             for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
-    
     return result
