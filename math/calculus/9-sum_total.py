1abba22ece8b6fc4aaced88a8fee71f2862e1df2
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-04 18:42:30 -0500

    create task9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,9 +2,9 @@
 
 
 def summation_i_squared(n):
-    if n != int(n):
+    if n != int(n) or n < 1:
         return None
     elif n == 1:
-        return 1
+        return 1  # base case: sum of 1^2 is 1
     else:
-        return n**2 + summation_i_squared(n-1)
+        return n**2 + summation_i_squared(n-1)  
