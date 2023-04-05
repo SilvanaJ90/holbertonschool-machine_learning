8d482dd9d449d799615bb080e2239fb0e4f8fc63
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-05 16:57:18 -0500

    create-poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Doc """
+import numpy as np
+
+
+class Poisson:
+    """ Class Poisson """
+    def __init__(self, data=None, lambtha=1.):
+        self.data = data
+        self.lambtha = lambtha
+        
+    def lambtha(self, data):
+        lambtha = float(lambtha)
+           
+        if not data:
+            data = lambtha
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            lambtha * data
+            if data < 2:
+                raise ValueError("data must contain multiple values")
+        return lambtha
