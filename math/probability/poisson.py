7fe73b565daed99d57bda7d54eeffba087d20115
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-06 17:29:48 -0500

    create task1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -21,8 +21,7 @@ class Poisson:
         """ Calculates the value of the PMF """
         e = 2.7182818285
         factorial = 1
-        if isinstance(k, int):
-            k = int(k)
+        k = int(k)
         if k < 0:
             return 0
         for i in range(1, k+1):
