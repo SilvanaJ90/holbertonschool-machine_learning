a2f1adbd12e741dd8543689f44cee0cba55db67f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-06 17:22:48 -0500

    create task1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,6 +19,7 @@ class Poisson:
 
     def pmf(self, k):
         """ Calculates the value of the PMF """
+        e = 2.7182818285
         factorial = 1
         if isinstance(k, int):
             k = int(k)
@@ -26,5 +27,4 @@ class Poisson:
             return 0
         for i in range(1, k+1):
             factorial *= i
-        pmp = (self.lambtha**k) * (2.7182818285**(- self.lambtha)) / factorial
-        return pmp
+        return (self.lambtha**k) * (e**(- self.lambtha)) / factorial
