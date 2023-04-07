3d16a8ec8d2276530b485921bb030fc623a50a18
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-07 18:59:45 -0500

    create task4

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -27,7 +27,7 @@ class Exponential:
 
     def cdf(self, x):
         """ Calculates the value of the CDF """
-        x = int(x)
+        x = float(x)
         if x < 0:
             return 0
         cdf = 0
