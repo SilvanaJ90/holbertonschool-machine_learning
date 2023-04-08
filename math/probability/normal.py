647c5ff42337ea8b06e2e8b2fbba66b6e5f35b31
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-07 21:04:36 -0500

    create-task7

---
+++ math/probability/normal.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Doc """
+
+
+class Normal:
+    """ Class Normal """
+    def __init__(self, data=None, mean=0., stddev=1.):
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum((x - self.mean)**2 for x in data) / len(data))**(1/2)
