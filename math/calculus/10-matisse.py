420ce10ea82390aba29cb8aa6676acb5f5afa9f8
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-04-12 21:08:47 -0500

    create-task10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,12 +9,12 @@ def poly_derivative(poly):
     if not isinstance(poly, list) or len(poly) == 0:
         return None
     if len(poly) == 1:
-        return 0
+        return [0]
     result = []
     for i in range(1, len(poly)):
         coeff = i * poly[i]
         result.append(coeff)
 
     if sum(result) == 0:
-        return 0
+        return [0]
     return result
