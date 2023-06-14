c86269a56fab8ff7543b534e7fec617ae384000f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-06-13 21:11:31 -0500

    create task9

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Doc """
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class NeuralNetwork"""
+    def __init__(self, nx, nodes):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if (nx < 1):
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if (nodes < 1):
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__2 = 0
+        self.__A2 = 0
