1d8c00096a5d82c27b8353542feb8e830b981284
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-06-13 21:31:18 -0500

    create task9

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -6,21 +6,21 @@ import numpy as np
 class NeuralNetwork:
     """ Class NeuralNetwork"""
     def __init__(self, nx, nodes):
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if (nx < 1):
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(nodes) is not int:
+        if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
-        if (nodes < 1):
+        if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.__W1 = np.random.randn(nodes, nx)
-        self.__b1 = np.zeros((nodes, 1))
-        self.__A1 = 0
-        self.__W2 = np.random.randn(1, nodes)
-        self.__b2 = 0
-        self.__A2 = 0
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
 
         @property
         def W1(self):
