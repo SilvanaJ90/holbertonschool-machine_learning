db779caa71e2b8476305c510bbb913d4ed62b904
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-07-04 18:40:24 -0500

    create task tensorflow

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-import tensorflow as tf
 """ Function create_placeholders """
+
+import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-    """ Doc """
+    """
+        Fuction return placeholders x and y
+        for the neural network
+        nx: the number of feature columns in our data
+        classes: the number of classes in our classifier
+        Returns: placeholders named x and y, respectively
+        x is the placeholder for the input data to the neural network
+        y is the placeholder for the one-hot labels for the input data
+    """
+
     x = tf.placeholder("float", (None, nx), name='x')
     y = tf.placeholder("float", (None, classes), name='y')
     return x, y
