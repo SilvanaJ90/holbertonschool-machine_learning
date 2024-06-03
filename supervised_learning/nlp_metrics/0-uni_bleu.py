225a0a2099531b6cb0f3dbffc7f76df4718a38cb
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-06-03 10:23:11 -0500

    update

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -11,19 +11,21 @@ def uni_bleu(references, sentence):
     Returns: the unigram BLEU score
 
     """
+    # Calculate precision
     precision = 0
-    sentence_len = len(sentence)
-    closest_ref_len = min(len(reference) for reference in references)
-
     for word in sentence:
         max_word_count = max(reference.count(word) for reference in references)
         precision += max_word_count
-
     precision /= len(sentence)
 
+    # Calculate brevity penalty
+    closest_ref_len = min(len(reference) for reference in references)
+    sentence_len = len(sentence)
     brevity_penalty = 1
     if sentence_len < closest_ref_len:
         brevity_penalty = np.exp(1 - (closest_ref_len / sentence_len))
 
+    # Calculate BLEU score
     bleu_score = brevity_penalty * precision
+
     return bleu_score
