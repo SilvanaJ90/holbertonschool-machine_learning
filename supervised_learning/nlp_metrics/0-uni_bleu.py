dffe854505f915c2c7717eaabec9c0e36faa4777
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-06-03 10:18:26 -0500

    update

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """  that calculates the unigram BLEU score for a sentence: """
+import numpy as np
 
 
 def uni_bleu(references, sentence):
@@ -10,3 +11,15 @@ def uni_bleu(references, sentence):
     Returns: the unigram BLEU score
 
     """
+    word_max_count = {}
+    for word in set(sentence):
+        for reference in references:
+            reference_word_count = reference.count(word)
+            if reference_word_count > word_max_count.get(word, 0):
+                word_max_count[word] = reference_word_count
+    min_reference_len = min(len(reference) for ref in reference)
+    sentence_len = len(sentence)
+    brevity_penalty = 1 if (sentence_len > min_reference_len) \
+        else np.exp(1 - (min_reference_len / sentence_len))
+
+    return np.exp(1 - (min_reference_len / sentence_len))
