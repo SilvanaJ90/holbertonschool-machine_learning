f32cab135024ea0b5b6a335bafd70a619d49e854
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-06-03 08:47:03 -0500

    update task 0

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ that creates a bag of words embedding matrix: """
-from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+from collections import Counter
 
 
 def bag_of_words(sentences, vocab=None):
@@ -15,3 +16,20 @@ def bag_of_words(sentences, vocab=None):
         - features is a list of the features used for embeddings
         You are not allowed to use genism library.
     """
+    tokenized_sentences = [sentence.lower().split() for sentence in sentences]
+
+    # Get vocabulary if not provided
+    if vocab is None:
+        all_words = [word for sentence in tokenized_sentences for word in sentence]
+        vocab = list(set(all_words))
+
+    # Initialize embeddings matrix
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+
+    # Fill the embeddings matrix
+    for i, sentence in enumerate(tokenized_sentences):
+        word_counts = Counter(sentence)
+        for j, word in enumerate(vocab):
+            embeddings[i, j] = word_counts[word]
+
+    return embeddings, vocab
