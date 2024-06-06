e7f45b9372d999d4bfc3245c3df536806ab35ada
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-06-05 22:27:47 -0500

    create task 9

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """  to create the decoder for a transformer: """
+
 import tensorflow as tf
-import numpy as np
-from tensorflow.keras.layers import Layer, Embedding, Dropout
-EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
-class Encoder(Layer):
+class Encoder(tf.keras.layers.Layer):
     """ class Encoder """
     def __init__(
             self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
@@ -16,11 +15,10 @@ class Encoder(Layer):
 
         self.N = N
         self.dm = dm
-        self.embedding = Embedding(input_vocab, dm)
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(
-            dm, h, hidden, drop_rate) for _ in range(N)]
-        self.dropout = Dropout(drop_rate)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
         """ Doc """
