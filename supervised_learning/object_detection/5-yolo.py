d70c182801a93c020ed6f22e7daa95d4ef44779b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-06-22 22:39:38 -0500

    create task 5

--- supervised_learning/object_detection/5-yolo.py
+++ supervised_learning/object_detection/5-yolo.py
@@ -158,17 +158,17 @@ class Yolo:
 
     def preprocess_images(self, images):
         """ Doc """
+        input_h, input_w = self.input_h, self.input_w
         pimages = []
         image_shapes = []
-        input_h = self.model.input.shape[2].value
-        input_w = self.model.input.shape[1].value
 
         for image in images:
             image_shapes.append(image.shape[:2])
-            resized = cv2.resize(image, dsize=(input_w, input_h),
-                                 interpolation=cv2.INTER_CUBIC)
-            rescaled = resized / 255
-            pimages.append(rescaled)
+            resized_image = cv2.resize(
+                image, (input_w, input_h), interpolation=cv2.INTER_CUBIC)
+            pimage = resized_image / 255
+            pimages.append(pimage)
+
         pimages = np.array(pimages)
         image_shapes = np.array(image_shapes)
 
