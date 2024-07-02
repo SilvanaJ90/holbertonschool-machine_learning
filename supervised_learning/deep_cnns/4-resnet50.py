1a016c5bd36b7719ebe8e78a58742f670abdb77b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-01 19:10:21 -0500

    update tsk 4

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ t builds the ResNet-50 architecture """
-import tensorflow.keras as K
 
+from tensorflow import keras as K
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
 
