173366b6bf2b5a8ec10ffc1d2df5def576ec585f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-22 15:31:49 -0500

    create task 3

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,4 +7,13 @@ df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 # YOUR CODE HERE
 
+#   Convert the timestamp values to datatime values
+df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='s')
+
+# Rename the column Timestamp to Datetime
+df = df.rename(columns={'Timestamp': 'Datetime'})
+
+# Display only the Datetime and Close columns
+df = df[['Datetime', 'Close']]
+
 print(df.tail())
