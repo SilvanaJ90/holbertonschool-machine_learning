40ff306ec0aa392d706211cc6de6e5b43283371b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-23 16:12:28 -0500

    create task 0

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    By using the Swapi API, create a method that returns the list of
+    ships that can hold a given number of passengers:
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    """
+    url = "https://swapi.dev/api/starships/"
+    ships = []
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+        for ship in data['results']:
+            try:
+                passengers = ship['passengers'].replace(',', '')
+                if passengers.isdigit() and int(passengers) >= passengerCount:
+                    ships.append(ship['name'])
+            except (ValueError, TypeError):
+                continue
+        url = data['next']
+
+    return ships
+
+if __name__ == "__main__":
+    ships = availableShips(4)
+    for ship in ships:
+        print(ship)
---
+++ pipeline/apis/1-sentience.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    By using the Swapi API, create a method that returns the list
+    of names of the home planets of all sentient species.
+"""
+
+
+def sentientPlanets():
+    """
+    sentient type is either in the classification
+    or designation attributes.
+    """
