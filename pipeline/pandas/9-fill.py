3620aedee34a5d0831b63b40846cd7ad0246df28
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-22 20:02:09 -0500

    create tasks

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -6,6 +6,20 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 # YOUR CODE HERE
+# The column Weighted_Price should be removed
+df = df.drop(columns=['Weighted_Price'])
+
+# missing values in Close should be set to the previous row value
+df['Close'] = df['Close'].ffill()
+df['High'] = df['High'].fillna(df['Close'])
+
+# missing values in High, Low, Open should be set to the same row’s Close value
+df['Low'] = df['Low'].fillna(df['Close'])
+df['Open'] = df['Open'].fillna(df['Close'])
+
+# missing values in Volume_(BTC) and Volume_(Currency) should be set to 0
+df[['Volume_(BTC)', 'Volume_(Currency)']] = \
+    df[['Volume_(BTC)', 'Volume_(Currency)']].fillna(0)
 
 print(df.head())
 print(df.tail())
