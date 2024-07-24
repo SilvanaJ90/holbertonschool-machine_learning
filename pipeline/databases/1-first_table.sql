1ecfc5367668bc7a89c67346c801371c6ec97cc7
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-23 20:22:14 -0500

    create task 1

--- pipeline/databases/1-first_table.sql
+++ pipeline/databases/1-first_table.sql
@@ -1,6 +1,5 @@
 --- Write a script that creates a table called first_table in the current database in your MySQL server.
 
-USE db_0;
 CREATE TABLE IF NOT EXISTS first_table (
     id INT PRIMARY KEY,
     name VARCHAR(256)
