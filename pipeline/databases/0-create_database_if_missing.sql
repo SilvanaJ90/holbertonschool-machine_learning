802542f61987780ab460ab68544e6bd6334ad961
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-23 20:11:47 -0500

    update

--- pipeline/databases/0-create_database_if_missing.sql
+++ pipeline/databases/0-create_database_if_missing.sql
@@ -1,6 +1,3 @@
 -- Create database + user if doesn't exist
 CREATE DATABASE IF NOT EXISTS db_0;
-CREATE USER IF NOT EXISTS 'db_0_dev'@'localhost' IDENTIFIED BY 'db_0_pwd';
-GRANT ALL ON `db_0`.* TO 'db_0_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'db_0_dev'@'localhost';
-FLUSH PRIVILEGES;
+
