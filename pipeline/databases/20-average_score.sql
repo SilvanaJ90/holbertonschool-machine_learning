0da1f66c9829d52d0d38ec2b0b11406afd22bfed
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-27 15:17:08 -0500

    create tsk 20

---
+++ pipeline/databases/20-average_score.sql
@@ -0,0 +1,24 @@
+-- Write a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+-- Requirements:
+
+    -- Procedure ComputeAverageScoreForUser is taking 1 input:
+        -- user_id, a users.id value (you can assume user_id is linked to an existing users)
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+    
+    -- Calculate the average score
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+    
+    -- Update the average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END$$
+
+DELIMITER ;
