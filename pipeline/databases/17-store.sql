20c220efc4adf0af922ec04a62a79adc8dde810f
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-27 11:12:00 -0500

    create task 17

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,2 +1,14 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
-x
+
+DELIMITER //
+
+CREATE TRIGGER item_decreases_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+
+END //
+DELIMITER ;
