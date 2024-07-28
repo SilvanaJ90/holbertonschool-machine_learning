dbb25599eaa2647a035496795f8365765d1199f0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-27 21:24:18 -0500

    update

---
+++ pipeline/databases/34-log_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    Write a Python script that provides some stats
+    about Nginx logs stored in MongoDB:
+"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """
+    Provides some statistics about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    # Number of documents in the collection
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
+
+    # Number of documents with each method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    # Number of documents with method GET and path /status
+    status_check_count = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{status_check_count} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
