e6b872cb21a668596d31403d44de005934ece154
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-27 21:00:24 -0500

    create task 31

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Write a Python function that inserts a new document in a collection based on kwargs """
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    mongo_collection will be the pymongo collection object
+    Returns the new _id
+    """
+
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
