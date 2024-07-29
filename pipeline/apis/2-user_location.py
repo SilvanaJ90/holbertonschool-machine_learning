8065dd1c5b76fcd3514014b40f5e48695dad3181
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-07-29 14:42:56 -0500

    update

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
-"""
+import requests
+import sys
+from datetime import datetime
 
-"""
+def get_user_location(url):
+    response = requests.get(url)
 
+    if response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
+        reset_datetime = datetime.fromtimestamp(reset_time)
+        minutes_left = int((reset_datetime - datetime.now()).total_seconds() / 60)
+        print(f"Reset in {minutes_left} min")
+    elif response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('location', 'Location not available'))
+    else:
+        print("Unexpected error")
 
-"""
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+        sys.exit(1)
 
-The user is passed as first argument of the script with the full API URL, example: ./2-user_location.py https://api.github.com/users/holbertonschool
-If the user doesn’t exist, print Not found
-If the status code is 403, print Reset in X min where X is the number of minutes from now and the value of X-Ratelimit-Reset
-Your code should not be executed when the file is imported (you should use if __name__ == '__main__':)
-
-"""
+    url = sys.argv[1]
+    get_user_location(url)
