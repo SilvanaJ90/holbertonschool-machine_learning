6deb5ab8dee666f1423e94c495e3706d6d57c496
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-05 17:24:58 -0500

    update

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -12,20 +12,18 @@ if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
 
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            location = response.json().get("location")
-            if location:
-                print(location)
-            else:
-                print("Location not available")
-        elif response.status_code == 403:
-            X_Ratelimit_Reset = response.headers.get("X-Ratelimit-Reset")
-            now_time = datetime.now().timestamp()
-            diff = (int(X_Ratelimit_Reset) - now_time) / 60
-            print("Reset in {} min".format(int(diff)))
+    response = requests.get(url)
+    if response.status_code == 200:
+        location = response.json().get("location")
+        if location:
+            print(location)
         else:
-            print("Not found")
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+            print("Location not available")
+    elif response.status_code == 403:
+        X_Ratelimit_Reset = response.headers.get("X-Ratelimit-Reset")
+        now_time = datetime.now().timestamp()
+        diff = (int(X_Ratelimit_Reset) - now_time) / 60
+        print("Reset in {} min".format(int(diff)))
+    else:
+        print("Not found")
+
