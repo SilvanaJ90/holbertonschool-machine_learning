da943839c44ac71cc4df82ef314e218ba9553d36
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-05 17:15:40 -0500

    create task 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -11,4 +11,13 @@ if __name__ == '__main__':
     """ Doc """
     url = sys.argv[1]
     response = requests.get(url)
-    print(response)
+
+    if response.status_code == 200:
+        print(response.json()["location"])
+    elif response.status_code == 403:
+        X_Ratelimit_Reset = response.headers["X-Ratelimit-Reset"]
+        now_time = datetime.now().timestamp()
+        diff = (int(X_Ratelimit_Reset) - str(now_time)) / 60
+        print("Reset in {} min".format(int(diff)))
+    else:
+        print("Not found")
