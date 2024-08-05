c6bd130a490ed0f2c6e47fbada3b753d0bdd6aaa
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-05 13:45:06 -0500

    create task 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python3
-""" Doc """
+""" By using the GitHub API, write a script that
+    prints the location of a specific user:
+"""
 import requests
 import sys
 from datetime import datetime
 
-def get_user_location(url):
-    """ Doc """
-    response = requests.get(url)
-
-    if response.status_code == 404:
-        print("Not found")
-    elif response.status_code == 403:
-        reset_time = int(response.headers.get('X-Ratelimit-Reset', 0))
-        reset_datetime = datetime.fromtimestamp(reset_time)
-        minutes_left = int((reset_datetime - datetime.now()).total_seconds() / 60)
-        print(f"Reset in {minutes_left} min")
-    elif response.status_code == 200:
-        user_data = response.json()
-        print(user_data.get('location', 'Location not available'))
-    else:
-        print("Unexpected error")
 
 if __name__ == '__main__':
     """ Doc """
-    if len(sys.argv) != 2:
-        print("Usage: ./2-user_location.py <GitHub API URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    get_user_location(url)
+    response = requests.get(url)
+    print(response)
