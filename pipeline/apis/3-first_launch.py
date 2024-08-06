98013fcac0fa294974393e00ae79047cf7c77180
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-06 17:37:13 -0500

    update

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -7,39 +7,27 @@ import requests
 
 
 if __name__ == '__main__':
-    """ DOc """
     url = 'https://api.spacexdata.com/v4/launches/upcoming'
 
-    # Fetch the list of upcoming launches
     response = requests.get(url)
-    response.raise_for_status()
-    launches = response.json()
+    launches_sorted = sorted(response.json(), key=lambda x: x["date_unix"])
 
-    # Sort launches by date_unix
-    launches_sorted = sorted(launches, key=lambda x: x["date_unix"])
+    date_unix = launches_sorted[-1]["date_unix"]
 
-    # Get the first launch
-    first_launch = launches_sorted[0]
+    for launch in response.json():
+        if launch["date_unix"] == date_unix:
+            launch_name = launch["name"]
+            date = launch["date_local"]
+            rocket_id = launch["rocket"]
+            launchpad_id = launch["launchpad"]
+            break
 
-    # Extract details of the first launch
-    launch_name = first_launch["name"]
-    date = first_launch["date_local"]
-    rocket_id = first_launch["rocket"]
-    launchpad_id = first_launch["launchpad"]
+    rock_name = requests.get("https://api.spacexdata.com/v4/rockets/"
+                             + rocket_id).json()["name"]
+    launchpad_name = requests.get("https://api.spacexdata.com/v4/launchpads/"
+                                  + launchpad_id).json()["name"]
+    launchpad_loc = requests.get("https://api.spacexdata.com/v4/launchpads/"
+                                 + launchpad_id).json()["locality"]
 
-    # Fetch rocket details
-    rocket_response = requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
-    rocket_response.raise_for_status()
-    rocket_name = rocket_response.json()["name"]
-
-    # Fetch launchpad details
-    launchpad_response = requests.get(f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
-    launchpad_response.raise_for_status()
-    launchpad = launchpad_response.json()
-    launchpad_name = launchpad["name"]
-    launchpad_loc = launchpad["locality"]
-
-    # Print the result in the specified format
     print("{} ({}) {} - {} ({})".format(
-        launch_name, date, rocket_name, launchpad_name, launchpad_loc))
-
+        launch_name, date, rock_name, launchpad_name, launchpad_loc))
