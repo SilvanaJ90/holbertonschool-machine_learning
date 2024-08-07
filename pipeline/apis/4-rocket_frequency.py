7c6377f582bd28fef6b9e2de9e5c387a1a0752f3
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-06 20:34:16 -0500

    update

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -3,24 +3,24 @@
 
 """
 import requests
+import json
 
 
 if __name__ == '__main__':
-    url = 'https://api.spacexdata.com/v4/launches'
-     
+    url = 'https://api.spacexdata.com/v3/launches'
+
     response = requests.get(url)
-    launches_sorted = sorted(response.json(), key=lambda x:x["date_unix"])
+    launches = json.loads(response.content)
 
-    date_unix = launches_sorted[-1]["date_unix"]
+    rocket = {}
 
-    for launch in response.json():
-        if launch["date_unix"] == date_unix:
-            launch_name = launch["name"]
-            rocket_id = lauch["date_local"]
-            launchpad_id = launch["lauchpad"]
-            break
-    
-    rock_name = requests.get("https://api.spacexdata.com/v4/rockets/" + rocket_id).json()["name"]
-    launchpad_name = requests.get("https://api.spacexdata.com/v4/launchpads/" + launchpad_id).json()["name"]
-    launchpad_locality = requests.get("https://api.spacexdata.com/v4/launchpads/" + launchpad_id).json()["locality"]
-    print(response)
+    for launch in launches:
+        rocket_name = launch['rocket']['rocket_name']
+        if rocket_name not in rocket:
+            rocket[rocket_name] = 1
+        else:
+            rocket[rocket_name] += 1
+    sorted_rockets = sorted(
+        rocket.items(), key=lambda x: x[1], reverse=True)
+    for rocket, count in sorted_rockets:
+        print(f"{rocket}: {count}")
