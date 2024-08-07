a9477832f900d0acc6d47f2cfba62ed4774e26c0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-06 20:49:11 -0500

    update

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -10,24 +10,29 @@ if __name__ == '__main__':
     url = 'https://api.spacexdata.com/v4/launches/upcoming'
 
     response = requests.get(url)
-    launches_sorted = sorted(response.json(), key=lambda x: x["date_unix"])
+    response.raise_for_status()
+    launches = response.json()
 
-    date_unix = launches_sorted[-1]["date_unix"]
+    launches_sorted = sorted(launches, key=lambda x: x["date_unix"])
 
-    for launch in response.json():
-        if launch["date_unix"] == date_unix:
-            launch_name = launch["name"]
-            date = launch["date_local"]
-            rocket_id = launch["rocket"]
-            launchpad_id = launch["launchpad"]
-            break
+    first_launch = launches_sorted[0]
 
-    rock_name = requests.get("https://api.spacexdata.com/v4/rockets/"
-                             + rocket_id).json()["name"]
-    launchpad_name = requests.get("https://api.spacexdata.com/v4/launchpads/"
-                                  + launchpad_id).json()["name"]
-    launchpad_loc = requests.get("https://api.spacexdata.com/v4/launchpads/"
-                                 + launchpad_id).json()["locality"]
+    launch_name = first_launch["name"]
+    date = first_launch["date_local"]
+    rocket_id = first_launch["rocket"]
+    launchpad_id = first_launch["launchpad"]
+
+    rocket_response = requests.get(
+        f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
+    rocket_response.raise_for_status()
+    rocket_name = rocket_response.json()["name"]
+
+    launchpad_response = requests.get(
+        f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
+    launchpad_response.raise_for_status()
+    launchpad = launchpad_response.json()
+    launchpad_name = launchpad["name"]
+    launchpad_loc = launchpad["locality"]
 
     print("{} ({}) {} - {} ({})".format(
-        launch_name, date, rock_name, launchpad_name, launchpad_loc))
+        launch_name, date, rocket_name, launchpad_name, launchpad_loc))
