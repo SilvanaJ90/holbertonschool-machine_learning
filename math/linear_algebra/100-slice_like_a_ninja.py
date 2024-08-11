4198d5fd7ebb13af6dcf517b1c9c3cb229ea0371
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-11 13:04:16 -0500

    create task 100

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+""" that slices a matrix along specific axes: """
 
 
 def np_slice(matrix, axes={}):
-    """ that slices a matrix along specific axes: """
+    """
+    You can assume that matrix is a numpy.ndarray
+    You must return a new numpy.ndarray
+    axes is a dictionary where the key is an axis
+    to slice along and the value is a tuple representing the slice to make
+    along that axis You can assume that axes represents a valid slice
+    Hint
+    """
     slices = [slice(None)] * matrix.ndim
 
     # Update slices based on the axes dictionary
