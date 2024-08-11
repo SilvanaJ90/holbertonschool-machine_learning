f09b57ff29627131e2172acac6d9b54c59451dcf
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-11 11:46:50 -0500

    update

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from nltk.stem import WordNetLemmatizer
 import string
+import nltk
+nltk.download('wordnet')
+nltk.download('stopwords')
 
 def preprocess(sentence):
     # Convertir a minúsculas y eliminar puntuación
