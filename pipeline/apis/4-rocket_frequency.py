4819c86a991cce8b0ad7a0f6f8b0c6e4f64f6554
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-10 19:30:47 -0500

    update task 4

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -5,6 +5,7 @@
 """
 import requests
 
+
 if __name__ == '__main__':
     url = 'https://api.spacexdata.com/v4/launches'
 
@@ -14,8 +15,10 @@ if __name__ == '__main__':
     rockets = {}
 
     for launch in launches:
-        rocket_name = launch['rocket']
-        if rocket_name not in rockets:
+        rocket_id = launch['rocket']
+        if rocket_id not in rockets:
+            rocket_response = requests.get('https://api.spacexdata.com/v4/rockets/' + rocket_id)
+            rocket_name = rocket_response.json()['name']
             rockets[rocket_name] = 1
         else:
             rockets[rocket_name] += 1
