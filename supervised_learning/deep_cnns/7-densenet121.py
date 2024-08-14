9686256dd3e40b0214eb5a69c28d3f465c2a4f55
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-14 04:34:36 -0500

    update task 4

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ builds the DenseNet-121 architecture """
-import tensorflow.keras as K
+from tensorflow import keras as K
+dense_block = __import__('5-dense_block').dense_block
+transition_layer = __import__('6-transition_layer').transition_layer
 
 
 def densenet121(growth_rate=32, compression=1.0):
