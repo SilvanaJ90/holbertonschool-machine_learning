54b6a1ad946f5ff24ee43fcebc7df6de4249aa46
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-14 08:53:28 -0500

    update

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -5,17 +5,17 @@ import tensorflow.compat.v1 as tf
 
 def evaluate(X, Y, save_path):
     """ Doc """
-    x, y = create_placeholders(X.shape[1], Y.shape[1])
-    y_pred = forward_prop(
-        x, [256, 256, Y.shape[1]], [tf.nn.tanh, tf.nn.tanh, None])
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-
-    saver = tf.train.Saver()
-
     with tf.Session() as sess:
-        saver.restore(sess, save_path)
-        Y_pred, acc, cost = sess.run(
-            [y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        server = tf.train.import_meta_grap(save_path + ".meta")
+        server.restore(sess, save_path)
 
-    return Y_pred, acc, cost
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection('y')[0]
+        y_pred = tf.get_collection('y_pred')[0]
+        accuracy = tf.get_collection('accuracy')[0]
+        loss = tf.get_collection('loss')[0]
+
+        prediction = sess.run(y_pred, feed_dict=-{x: X, Y: y})
+        accuracy = sess.run(accuracy, feed_dict=-{x: X, Y: y})
+        loss = sess.run(loss, feed_dict=-{x: X, Y: y})
+    return prediction, accuracy, loss
