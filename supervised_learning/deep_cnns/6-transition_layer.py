cd526a309b5b2835903e8377631a48daef3abba9
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-14 05:28:14 -0500

    update task 6

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -5,8 +5,6 @@ import tensorflow.keras as K
 
 def transition_layer(X, nb_filters, compression):
     """
-
-
     X is the output from the previous layer
     nb_filters is an integer representing the number of filters in X
     compression is the compression factor for the transition layer
@@ -16,7 +14,21 @@ def transition_layer(X, nb_filters, compression):
     rectified linear activation (ReLU), respectively
     Returns: The output of the transition layer and the number
     of filters within the output, respectively
-
-
-
     """
+    initializer = K.initializers.he_normal(seed=0)
+
+    # Batch Normalization + ReLU
+    bn = K.layers.BatchNormalization()(X)
+    relu = K.layers.Activation('relu')(bn)
+
+    # 1x1 Convolution
+    nb_filters = int(nb_filters * compression)
+    conv = K.layers.Conv2D(
+        nb_filters, (1, 1),
+        padding='same', kernel_initializer=initializer)(relu)
+
+    # Average Pooling (2x2)
+    avg_pool = K.layers.AveragePooling2D(
+        pool_size=(2, 2), strides=(2, 2), padding='same')(conv)
+
+    return avg_pool, nb_filters
