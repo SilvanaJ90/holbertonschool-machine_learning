d3c4c58b0417cf9a87fa7732f07ac21162b3d1d3
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-14 10:01:15 -0500

    update

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -6,16 +6,20 @@ import tensorflow.compat.v1 as tf
 def evaluate(X, Y, save_path):
     """ Doc """
     with tf.Session() as sess:
-        server = tf.train.import_meta_grap(save_path + ".meta")
-        server.restore(sess, save_path)
+        # Restore the saved model
+        saver = tf.train.import_meta_graph(save_path + ".meta")
+        saver.restore(sess, save_path)
 
+        # Retrieve the necessary elements from the graph's collections
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         y_pred = tf.get_collection('y_pred')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
 
-        prediction = sess.run(y_pred, feed_dict=-{x: X, Y: y})
-        accuracy = sess.run(accuracy, feed_dict=-{x: X, Y: y})
-        loss = sess.run(loss, feed_dict=-{x: X, Y: y})
-    return prediction, accuracy, loss
+        # Evaluate predictions, accuracy, and loss
+        prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
+        accuracy_value = sess.run(accuracy, feed_dict={x: X, y: Y})
+        loss_value = sess.run(loss, feed_dict={x: X, y: Y})
+    
+    return prediction, accuracy_value, loss_value
