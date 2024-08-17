2ca96057638bcbbf38790bfb18b56ec2d397133b
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-17 18:35:14 -0500

    update

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -27,28 +27,55 @@ def lenet5(X):
         use the relu activation function
         you may import tensorflow.keras as K
     """
+    # Create a Sequential model
     model = K.Sequential()
+
+    # Convolutional layer 1
     model.add(K.layers.Conv2D(
-        6, 5, activation='relu',
-        kernel_initializer='he_normal',
-        padding='same', input_shape=X.shape[1:]))
-    model.add(K.layers.MaxPooling2D())
-    model.add(
-        K.layers.Conv2D(
-            16, 5, activation='relu', kernel_initializer='he_normal'))
-    model.add(K.layers.MaxPooling2D())
+        filters=6, kernel_size=(5, 5),
+        padding='same', activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=0),
+        input_shape=X.shape[1:]
+    ))
+
+    # Max pooling layer 1
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Convolutional layer 2
+    model.add(K.layers.Conv2D(
+        filters=16, kernel_size=(5, 5),
+        padding='valid', activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=0)
+    ))
+
+    # Max pooling layer 2
+    model.add(K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Flatten the output from the previous layer
     model.add(K.layers.Flatten())
-    model.add(
-        K.layers.Dense(
-            120, activation='relu', kernel_initializer='he_normal'))
-    model.add(
-        K.layers.Dense(
-            84, activation='relu', kernel_initializer='he_normal'))
-    model.add(
-        K.layers.Dense(
-            10, activation='softmax',
-            kernel_initializer='he_normal'))
-    model.compile(optimizer=K.optimizers.Adam(),
-                  loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Fully connected layer 1
+    model.add(K.layers.Dense(
+        units=120, activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=0)
+    ))
+
+    # Fully connected layer 2
+    model.add(K.layers.Dense(
+        units=84, activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=0)
+    ))
+
+    # Output layer
+    model.add(K.layers.Dense(
+        units=10, activation='softmax'
+    ))
+
+    # Compile the model
+    model.compile(
+        optimizer=K.optimizers.Adam(),
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
 
     return model
