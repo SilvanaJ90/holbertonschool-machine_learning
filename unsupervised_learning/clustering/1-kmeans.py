c23e560c3f2d9d891a153cf00cf6180eaeeb83c5
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-18 16:45:59 -0500

    update

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -12,30 +12,24 @@ def kmeans(X, k, iterations=1000):
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    # Setting min and max values per col
     n, d = X.shape
-    X_min = X.min(axis=0)
-    X_max = X.max(axis=0)
+    min_vals = X.min(axis=0)
+    max_vals = X.max(axis=0)
 
-    # Centroid
-    C = np.random.uniform(X_min, X_max, size=(k, d))
+    C = np.random.uniform(min_vals, max_vals, size=(k, d))
 
-    # Loop per the maximum number of iterations
     for i in range(iterations):
 
-        # initializes k centroids by selecting them from the data points
         centroids = np.copy(C)
         centroids_extended = C[:, np.newaxis]
 
-        # distances also know as euclidean distance
         distances = np.sqrt(((X - centroids_extended) ** 2).sum(axis=2))
-        # an array containing the index to the nearest centroid per each point
+
         clss = np.argmin(distances, axis=0)
 
-        # Assign all points to the nearest centroid
         for c in range(k):
             if X[clss == c].size == 0:
-                C[c] = np.random.uniform(X_min, X_max, size=(1, d))
+                C[c] = np.random.uniform(min_vals, max_vals, size=(1, d))
             else:
                 C[c] = X[clss == c].mean(axis=0)
 
