5ee399737f7a23e2da2d9c2b43961db5eb1b1c87
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-18 19:14:09 -0500

    update

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -10,6 +10,16 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
     doc
     """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None, None, None, None
+    if type(k) != int or k <= 0 or X.shape[0] < k:
+        return None, None, None, None, None
+    if type(iterations) != int or iterations <= 0:
+        return None, None, None, None, None
+    if type(tol) != float or tol < 0:
+        return None, None, None, None, None
+    if type(verbose) != bool:
+        return None, None, None, None, None
     # Initialize parameters
     pi, m, S = initialize(X, k)
     
