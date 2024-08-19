7c1ac4f6db4eaff414dbf1b514bf63d2efcff583
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-18 19:25:30 -0500

    update

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -24,26 +24,27 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     pi, m, S = initialize(X, k)
     
     log_likelihood_old = 0
+    i = 0
     
     for i in range(iterations):
         # Expectation step
-        g, _ = expectation(X, pi, m, S)
+        g, loglikelihood_new = expectation(X, pi, m, S)
         
+        if verbose is True and (i % 10 == 0):
+            print("Log Likelihood after {} iterations: {}".format(
+                i, loglikelihood_new.round(5)))
+        if abs(loglikelihood_new - log_likelihood_old) <= tol:
+            break
         # Maximization step
         pi, m, S = maximization(X, g)
-        
-        # Compute log likelihood
-        log_likelihood_new = np.sum(np.log(np.sum(g, axis=0)))
+        i += 1
+        log_likelihood_old = loglikelihood_new
+    g, loglikelihood_new = expectation(X, pi, m, S)
 
-        if log_likelihood_old is not None and np.abs(log_likelihood_new - log_likelihood_old) <= tol:
-            if verbose:
-                print(f"Log Likelihood after {i} iterations: {log_likelihood_new:.5f}")
-            break
-        
-        log_likelihood_old = log_likelihood_new
-        
-        # Print log likelihood every 10 iterations and last iteration if verbose
-        if verbose and (i % 10 == 0 or i == iterations - 1):
-            print(f"Log Likelihood after {i} iterations: {log_likelihood_new:.5f}")
+
+    if verbose is True:
+        print("Log Likelihood after {} iterations: {}".format(
+            i, loglikelihood_new.round(5)))
+
     
-    return pi, m, S, g, log_likelihood_new
+    return pi, m, S, g, loglikelihood_new
