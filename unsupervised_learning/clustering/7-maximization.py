1c2df5198a6adb3e5edd382ce7bba6295f3e74df
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-08-18 19:29:42 -0500

    update

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -6,17 +6,17 @@ import numpy as np
 
 def maximization(X, g):
     """
-    X is a numpy.ndarray of shape (n, d) containing the data set
-    g is a numpy.ndarray of shape (k, n) containing the posterior
-    probabilities for each data point in each cluster
-    You may use at most 1 loop
-    Returns: pi, m, S, or None, None, None on failure
-        pi is a numpy.ndarray of shape (k,) containing the
-        updated pi for each cluster
-        m is a numpy.ndarray of shape (k, d) containing the updated
-        centroid means for each cluster
-        S is a numpy.ndarray of shape (k, d, d) containing the updated
-        covariance matrices for each cluster
+        X is a numpy.ndarray of shape (n, d) containing the data set
+        g is a numpy.ndarray of shape (k, n) containing the posterior
+        probabilities for each data point in each cluster
+        You may use at most 1 loop
+        Returns: pi, m, S, or None, None, None on failure
+            pi is a numpy.ndarray of shape (k,) containing the
+            updated pi for each cluster
+            m is a numpy.ndarray of shape (k, d) containing the updated
+            centroid means for each cluster
+            S is a numpy.ndarray of shape (k, d, d) containing the updated
+            covariance matrices for each cluster
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-"""Perform the Expectation-Maximization algorithm for Gaussian Mixture Models (GMM)."""
+"""
+    Perform the Expectation-Maximization
+    algorithm for Gaussian Mixture Models (GMM).
+"""
 import numpy as np
 
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
+
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
@@ -12,24 +16,24 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None, None
-    if type(k) != int or k <= 0 or X.shape[0] < k:
+    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
         return None, None, None, None, None
-    if type(iterations) != int or iterations <= 0:
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None, None
-    if type(tol) != float or tol < 0:
+    if not isinstance(tol, float) or tol < 0:
         return None, None, None, None, None
-    if type(verbose) != bool:
+    if not isinstance(verbose, bool):
         return None, None, None, None, None
     # Initialize parameters
     pi, m, S = initialize(X, k)
-    
+
     log_likelihood_old = 0
     i = 0
-    
+
     for i in range(iterations):
         # Expectation step
         g, loglikelihood_new = expectation(X, pi, m, S)
-        
+
         if verbose is True and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {}".format(
                 i, loglikelihood_new.round(5)))
@@ -41,10 +45,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         log_likelihood_old = loglikelihood_new
     g, loglikelihood_new = expectation(X, pi, m, S)
 
-
     if verbose is True:
         print("Log Likelihood after {} iterations: {}".format(
             i, loglikelihood_new.round(5)))
 
-    
     return pi, m, S, g, loglikelihood_new
